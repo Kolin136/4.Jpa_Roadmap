215fb8f788ed2489469ee66197540c3dff1c13a9
Kolin136 <jsowb136@gmail.com>
2024-04-02 17:39:57 +0900

    "섹션6-2"

--- src/main/java/study/querydsl/repository/MemberRepository.java
+++ src/main/java/study/querydsl/repository/MemberRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import study.querydsl.entity.Member;
 
 @Repository
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long>,MemberRepositoryCustom {
 
   @Query("select m from Member m join m.team t where t.name = :name")
   List<Member> findMemberJoinTeam(@Param("name") String name);
